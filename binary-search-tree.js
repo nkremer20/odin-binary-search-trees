a0c7e89e359913d004e65af4881cba0e0c43101e
Noah Kremer <75767608+nkremer20@users.noreply.github.com>
2025-12-01 15:00:56 -0600

    create bst file and begin creating tree class

---
+++ binary-search-tree.js
@@ -0,0 +1,13 @@
+class Tree {
+    constructor(array) {
+        array = this.array;
+        root = NaN;
+    }
+
+    function buildTree(array) {
+        // Sort array
+
+        // Create BST
+    }
+
+}
